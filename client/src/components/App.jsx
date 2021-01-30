ba0876f9d038a5a0f8572e9f627a60eff05796be
akaaariii <caaari23@gmail.com>
2021-01-30 06:22:44 -0800

    init commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Header from './Header'
+const Landing = () => (<div>Landing</div>);
+const Dashboard = () => (<div>Dashboard</div>);
+const SurveyNew = () => (<div>SurveyNew</div>);
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Route path="/" exact component={Landing} />
+      <Route path="/surveys" exact component={Dashboard} />
+      <Route path="/surveys/new" component={SurveyNew} />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+
+import App from './components/App'
+
+
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.querySelector('#root'))
